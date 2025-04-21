9653359178053072af340e542872947c6916dfd6
Preetham Manjunatha <preethamam@gmail.com>
2025-04-21 03:37:44 -0700

    Cleaned up files.

--- Classification Examples/Python/tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-.picture_1:
-        #     picture_widget.labelText.text = "Current image"
-        # elif picture_widget == self.picture_2:
-        #     picture_widget.labelText.text = "Next image"  
-        # elif picture_widget == self.picture_past:
-        #     picture_widget.labelText.text = "Previous image"
